088d42b27a725345d2f8b7534ab9ee154dfbf56b
liezlsantos <lizzz_03@yahoo.com>
2014-02-18 20:56:18 +0800

    javascript to js

--- application/views/set/header.php
+++ application/views/set/header.php
@@ -1,8 +1,8 @@
 <?php include('check.php'); ?>
 	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
 		
-	<script src="<?php echo base_url('javascript/jquery-1.9.1.js');?>"></script>
-	<script src="<?php echo base_url('javascript/jquery-ui.js');?>"></script>
+	<script src="<?php echo base_url('js/jquery-1.9.1.js');?>"></script>
+	<script src="<?php echo base_url('js/jquery-ui.js');?>"></script>
 	<script>
 		$(document).ready(function() {
 			$( "#accordion" ).accordion({active: 0, heightStyle: "content"});
