20a37f56695677304467d0dca373160b18ca670e
liezlsantos <lizzz_03@yahoo.com>
2014-02-21 15:33:02 +0800

    login view 2

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -13,7 +13,7 @@
 				<?php echo str_replace('http', 'https', form_open('login/submit', array('onSubmit'=>true))); ?> 
 				<table cellpadding="3">
 					<tr><td colspan="2" align="center"><font color = red><?php echo validation_errors(); ?>&nbsp;</font></td></tr>
-					<tr><td colspan="2">For student used your student number (20xx-xxx)</td></tr>
+					<tr><td colspan="2">For student use your student number (20xx-xxx) as username</td></tr>
 					<tr><td>&nbsp;</td></tr>
 					<tr><td>Username <td><input type="text" name="username" size=25 required/> <br/>
 					<tr><td><br/></td></tr>
